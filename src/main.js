60165a6fc15acb9c2ff84d08c754eea21b182c68
xyf <425746853@qq.com>
2018-02-20 17:16:49 +0800

    search-input

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './common/stylus/index.styl';
+import vue_resourse from 'vue-resource';
+Vue.use(vue_resourse);
 new Vue({
   el: '#app',
   render: h => h(App)
